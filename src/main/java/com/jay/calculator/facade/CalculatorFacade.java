7aaa080094d075e90ddc3fc39f76d55a8f0e928f
jay.chen <55216443@qq.com>
2020-09-19 02:27:16 +0800

    jay: first commit

---
+++ src/main/java/com/jay/calculator/facade/CalculatorFacade.java
@@ -0,0 +1,15 @@
+package com.jay.calculator.facade;
+
+/**
+ * @author jay
+ * @comment this is the facade of calculator
+*/
+public interface CalculatorFacade {
+
+    /**
+     * to process command here
+     * @param String command
+     * @return void
+    */
+    void processCommand(String command);
+}
